597da5c48ea4e820d9977a1450e317de28be2e31
Alan Winkowski <walan@marvell.com>
2016-02-11 18:07:39 +0200

    mvpp2x: Modified initial copyright header
    Description:
    ------------
    Modified initial copyright header according to new
    header from 2016
    
    Change-Id: Ia7c033d671945f5f0045609f7bc0d89ec449a5ec
    Signed-off-by: Alan Winkowski <walan@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/27452
    Reviewed-by: Yuval Caduri <cyuval@marvell.com>
    Tested-by: Yuval Caduri <cyuval@marvell.com>

--- mv_pp2x.h
+++ mv_pp2x.h
@@ -1,29 +1,21 @@
 /*
- * Driver for Marvell PPv2x family network controllers.
- *
- * Copyright (C) 2015 Marvell
- *
- *.
- *.This program is free software; you can redistribute it and/or modify it
- *.under the terms and conditions of the GNU General Public License,
- *.version 2, as published by the Free Software Foundation.
- *
- *.This program is distributed in the hope it will be useful, but WITHOUT
- *.ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- *.FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
- *.more details.
- *
- *.You should have received a copy of the GNU General Public License along with
- *.this program; if not, write to the Free Software Foundation, Inc.,
- *.51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
- *
- *.The full GNU General Public License is included in this distribution in
- *.the file called "COPYING".
- *
- *
- * Contact Information:
- * Linux Network Driver <XXX.YYYY@marvell.com>
- */
+* ***************************************************************************
+* Copyright (C) 2016 Marvell International Ltd.
+* ***************************************************************************
+* This program is free software: you can redistribute it and/or modify it
+* under the terms of the GNU General Public License as published by the Free
+* Software Foundation, either version 2 of the License, or any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+* ***************************************************************************
+*/
+
 
 #ifndef _MVPP2_H_
 #define _MVPP2_H_
