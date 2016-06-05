8f84155e6a059bb836225294204799ab7e1f251d
Stefan Chulski <stefanc@marvell.com>
2016-06-05 12:01:29 +0300

    mvpp2x: add force link down and reset MAC to open/close port
    1. fix MAC reset issue
    2. show right status in ethtool
    
    Change-Id: Ie028325d6974a426593c08e45272be8d5ac7058e
    Reviewed-on: http://vgitil04.il.marvell.com:8080/30322
    Reviewed-by: Yuval Caduri <cyuval@marvell.com>
    Tested-by: Yuval Caduri <cyuval@marvell.com>

--- mv_pp2x_main.c
+++ mv_pp2x_main.c
@@ -2707,8 +2707,11 @@ static int mvcpn110_mac_hw_init(struct mv_pp2x_port *port)
 	struct mv_mac_data *mac = &port->mac_data;
 	int gop_port = mac->gop_index;
 
-	if (mac->flags & MV_EMAC_F_INIT)
+	if (mac->flags & MV_EMAC_F_INIT) {
+		mv_gop110_gmac_reset(gop, gop_port, UNRESET);
+		mv_gop110_force_link_mode_set(gop, mac, false, false);
 		return 0;
+		}
 
 	/* configure port PHY address */
 	mv_gop110_smi_phy_addr_cfg(gop, gop_port, mac->phy_addr);
